d63936082509c39dbd433b12a9e7aef78fd097be
tjbishnoi29@gmail.com <tejas2007@>
2022-06-07 15:13:54 +0530

    worked on them but the core dosn't work

--- public/indexJS.js
+++ public/indexJS.js
@@ -1,34 +1,101 @@
 function getVal() {
-    var inputval = document.querySelector('input').value;
+    let inputval = document.querySelector('input').value;
     replacethechar(inputval)
   }
   function replacethechar(inputval){
-     console.log(inputval)
-     var inputval = inputval.replace("a", "s");
-     var inputval = inputval.replace("b","r");
-     var inputval = inputval.replace("c","@");
-     var inputval = inputval.replace("d","j");
-     var inputval = inputval.replace("e","m");
-     var inputval = inputval.replace("f","=");
-     var inputval = inputval.replace("g","o");
-     var inputval = inputval.replace("h","z");
-     var inputval = inputval.replace("i","q");
-     var inputval = inputval.replace("j","#");
-     var inputval = inputval.replace("k","g");
-     var inputval = inputval.replace("l","x");
-     var inputval = inputval.replace("m","w");
-     var inputval = inputval.replace("n","*");
-     var inputval = inputval.replace("o","v");
-     var inputval = inputval.replace("p","<");
-     var inputval = inputval.replace("q","a");
-     var inputval = inputval.replace("r","+");
-     var inputval = inputval.replace("s","l");
-     var inputval = inputval.replace("t","y");
-     var inputval = inputval.replace("u","()");
-     var inputval = inputval.replace("v","d");
-     var inputval = inputval.replace("w",">");
-     var inputval = inputval.replace("x","f");
-     var inputval = inputval.replace("y","t");
-     var inputval = inputval.replace("z","");
+     console.log(inputval);
+     let l = inputval.length;
+     console.log(l);
+     for(let i = 0; i < l ; ){
+            if(inputval.charAt(i)=="a"){
+                inputval.replaceAt(i,"s");
+                i++;
+            }else if (inputval.charAt(i)=="b"){
+                inputval.replaceAt(i,"r");
+                i++;
+            }else if(inputval.charAt(i)=="c"){
+                inputval.replaceAt(i,"@");
+                i++;
+            }else if(inputval.charAt(i)=="d"){
+                inputval.replaceAt(i,"j");
+                i++;
+            }else if(inputval.charAt(i)=="e"){
+                inputval.replaceAt(i,"m");
+                i++;
+            }else if(inputval.charAt(i)=="f"){
+                inputval.replaceAt(i,"=")
+                i++;
+            }else if(inputval.charAt(i)=="g"){
+                inputval.replaceAt(i,"o");
+                i++;
+            }else if(inputval.charAt(i)=="h"){
+                inputval.replaceAt(i,"z");
+                i++;
+            }else if(inputval.charAt(i)=="i"){
+                inputval.replaceAt(i,"q");
+                i++;
+            }else if(inputval.charAt(i)=="j"){
+                inputval.replaceAt(i,"#");
+                i++;
+            }else if(inputval.charAt(i)=="k"){
+                inputval.replaceAt(i,"g");
+                i++;
+            }else if(inputval.charAt(i)=="l"){
+                inputval.replaceAt(i,"x");
+                i++;
+            }else if(inputval.charAt(i)=="m"){
+                inputval.replaceAt(i,"w");
+                i++;
+            }else if(inputval.charAt(i)=="n"){
+                inputval.replaceAt(i,"*");
+                i++;
+            }else if(inputval.charAt(i)=="o"){
+                inputval.replaceAt(i,"v");
+                i++;
+            }else if(inputval.charAt(i)=="p"){
+                inputval.replaceAt(i,"<");
+                i++;
+            }else if(inputval.charAt(i)=="q"){
+                inputval.replaceAt(i,"a");
+                i++;
+            }else if(inputval.charAt(i)=="r"){
+                inputval.replaceAt(i,"+");
+                i++;
+            }else if(inputval.charAt(i)=="s"){
+                inputval.replaceAt(i,"l");
+                i++;
+            }else if(inputval.charAt(i)=="t"){
+                inputval.replaceAt(i,"y");
+                
+                i++;
+            }else if(inputval.charAt(i)=="u"){
+                inputval.replaceAt(i,"()");
+                i++;
+            }else if(inputval.charAt(i)=="v"){
+                inputval.replaceAt(i,"d");
+                i++;
+            }else if(inputval.charAt(i)=="w"){
+                inputval.replaceAt(i,">");
+                i++;
+            }else if(inputval.charAt(i)=="x"){
+                inputval.replaceAt(i,"f");
+                i++;
+            }else if(inputval.charAt(i)=="y"){
+                inputval.replaceAt(i,"t");
+                i++;
+            }else if(inputval.charAt(i)=="z"){
+                inputval.replaceAt(i,"Ω");
+                i++;    
+            }
+     }
+
      
+     console.log(inputval);
+  }
+  function count (string) {  
+    var count = {};
+    string.split('').forEach(function(s) {
+       count[s] ? count[s]++ : count[s] = 1;
+    });
+    return count;
   }
